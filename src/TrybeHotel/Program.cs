5f9454dbe6d305457b66acb04f4f439b8584734c
devpedroval <prmval@gmail.com>
2023-10-18 13:28:33 -0300

    iniciando projeto

--- src/TrybeHotel/Program.cs
+++ src/TrybeHotel/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using TrybeHotel.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
