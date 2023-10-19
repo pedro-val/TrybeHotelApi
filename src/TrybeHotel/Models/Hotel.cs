b7255898e6b20316a1fafd477465fe3ca6972bd2
devpedroval <prmval@gmail.com>
2023-10-19 18:38:10 -0300

    models e context configuradas

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -3,6 +3,15 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
+// CityId: Chave estrangeira para a model City (int) Cada hotel tem vários quartos. A propriedade de navegação para Room deve se chamar Rooms (anulável). Cada hotel tem apenas uma cidade. A propriedade de navegação para City deve se chamar City (anulável).
 public class Hotel {
+    public int HotelId { get; set; }
+    public string? Name { get; set; }
+    public string? Adress { get; set; }
+    public virtual List<Room>? Rooms { get; set; }
+
+    [ForeignKey("CityId")]
+    public int? CityId { get; set; }
+    public City? City { get; set; }
 
 }
--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,6 +4,17 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
-    public TrybeHotelContext() { }
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) 
+    { 
+    }
+
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=127.0.0.1;Database=trybe_hotel_db;User=sa;Password=TrybeHotel12!;TrustServerCertificate=true;";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 }
